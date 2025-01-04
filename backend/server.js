a4e95f45a1835ffd2ea02063a5cdaa6f51f3f308
karanpanchallll <129359582+karanpanchallll@users.noreply.github.com>
2025-01-05 00:02:26 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,10 @@
-import express from 'express';
-import cors from 'cors';
-import router from './routes/routes.js';
 import DBConnection from './database/db.js';
 import dotenv from 'dotenv';
 
+// Load environment variables from .env
 dotenv.config();
 
-const app = express();
-
-app.use(cors());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use('/', router);
-
-const PORT = process.env.PORT || 8000;
-
+// Connect to the database
 DBConnection();
 
-app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
+// Vercel automatically handles routing and listening, so no need for app.listen().
